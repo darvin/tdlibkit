0b41b6019d7fae8134e85a40bc3790c1c95df359
GGorAA <ggoraa1029@gmail.com>
2022-08-04 14:16:18 +0300

    fix: library not being dynamic

--- scripts/swift_package_generator.py
+++ scripts/swift_package_generator.py
@@ -15,6 +15,7 @@ let package = Package(
     products: [
         .library(
             name: "TDLibKit",
+            type: .dynamic,
             targets: ["TDLibKit"]),
     ],
     dependencies: [
